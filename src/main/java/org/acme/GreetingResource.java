3bc57400ccf3dbdc8dae7a61cb11402ffa5dde99
Robin De Mol <de.mol.robin@gmail.com>
2023-11-23 05:58:52 +0100

    remove unused imports

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,12 +1,9 @@
 package org.acme;
 
 import jakarta.enterprise.inject.spi.CDI;
-import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.container.ResourceContext;
 import jakarta.ws.rs.core.Context;
-import jakarta.ws.rs.core.MediaType;
 
 @Path("hello")
 public class GreetingResource {
